26645875ad897df6c7bd1550a4842a7b4d486df3
lZ3R0l <galaxyeye.1999@gmail.com>
2025-03-19 10:35:38 +0100

    added basic arithmetic op with variables

---
+++ main.js
@@ -0,0 +1,15 @@
+name = "Singh";
+age = 25;
+favecolor = "black";
+
+num1 = 5;
+num2 = 10;
+
+console.log('Hello, World!');
+
+console.log(`My name is ${name}, I am ${age} years old, and my favorite color is ${favecolor}.`);
+
+console.log(`${num1} + ${num2} = ${num1 + num2}`);
+console.log(`${num1} - ${num2} = ${num1 - num2}`);
+console.log(`${num1} * ${num2} = ${num1 * num2}`);
+console.log(`${num1} / ${num2} = ${num1 / num2}`);
